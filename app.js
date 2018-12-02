3f822e6693909639c9a850622ee3c2a67425a124
Radi Cho <radicho123@gmail.com>
2018-12-02 11:11:32 +0200

    Create hello world server with external IP in GCE.

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
-const express = require("express");
 const { Game, AI } = require("rsg-chess");
-const { performance } = require('perf_hooks');
+const { performance } = require("perf_hooks");
+
+const t0 = performance.now();
+runRSGChess();
+const t1 = performance.now();
+console.log("Call to RSG Chess' AI took " + (t1 - t0) + " milliseconds.");
 
 const runRSGChess = () => {
   const game = Game.prototype.initializeGame();
   const getAI = AI(5, game, true);
   console.log(getAI);
 };
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .send("Hello, world!")
-    .end();
-});
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  // var t0 = performance.now();
-  // runRSGChess();
-  // var t1 = performance.now();
-  // console.log("Call to RSG Chess' AI took " + (t1 - t0) + " milliseconds.");
-  console.log("App listening on port ", PORT);
-});

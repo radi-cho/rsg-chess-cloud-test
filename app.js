78cd97b60de0189ccf2f4470a77c8efe29e452a0
Radi Cho <radicho123@gmail.com>
2018-12-02 11:22:25 +0200

    Return the AI result with the response.

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+"use strict";
+
+const express = require("express");
 const { Game, AI } = require("rsg-chess");
 const { performance } = require("perf_hooks");
 
-const t0 = performance.now();
-runRSGChess();
-const t1 = performance.now();
-console.log("Call to RSG Chess' AI took " + (t1 - t0) + " milliseconds.");
+const app = express();
 
-const runRSGChess = () => {
+app.get("/", (_, res) => {
+  res
+  .status(200)
+  .send(runRSGChess(4))
+  .end();
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {});
+
+const runRSGChess = (depth) => {
+  const t0 = performance.now();
   const game = Game.prototype.initializeGame();
-  const getAI = AI(5, game, true);
-  console.log(getAI);
+  const getAI = AI(depth, game, true);
+  const t1 = performance.now();
+  return `Call to RSG Chess' AI with depth ${depth} took ${t1 - t0} milliseconds. \n Result: ${JSON.stringify(getAI)}`;
 };
